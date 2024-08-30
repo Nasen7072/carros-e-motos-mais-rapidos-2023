c049a7a0076f6035368b7c2a198c6c8fffeb28e1
Nasen7072 <148974860+Nasen7072@users.noreply.github.com>
2024-08-30 19:10:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.querySelector('.menu-hamburguer').addEventListener('click', function() {
-    document.querySelector('.menu-cascata').classList.toggle('show');
-});
 
-document.querySelector('.menu-sucos > a').addEventListener('click', function(event) {
-    event.preventDefault();
-    const sucosMenu = document.querySelector('.menu-sucos');
-    sucosMenu.classList.toggle('show-sucos');
-    if (sucosMenu.classList.contains('show-sucos')) {
-        sucosMenu.querySelector('ul').style.display = 'block';
-    } else {
-        sucosMenu.querySelector('ul').style.display = 'none';
-    }
-});
